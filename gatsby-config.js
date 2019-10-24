cb7d06efe798ae5a88835ae654449632f47ce1c4
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2019-10-23 18:10:48 -0700

    Equip gatsby-source-contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -27,6 +29,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     /* eslint-enable @typescript-eslint/camelcase */
